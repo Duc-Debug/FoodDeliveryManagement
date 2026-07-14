dea0fa0b68f570053149aede15971729eaebdf76
Duc <giapduc2299@gmail.com>
2026-07-14 15:39:33 +0700

    Xây dựng cơ bản hoàn thành luồng hoạt động của Customer, Có UI

--- src/main/java/com/delivery/ui/AppConsole.java
+++ src/main/java/com/delivery/ui/AppConsole.java
@@ -3,15 +3,18 @@ package com.delivery.ui;
 import java.util.List;
 import java.util.Scanner;
 
+import com.delivery.exception.NotFoundException;
 import com.delivery.exception.ValidationException;
 import com.delivery.model.Cart;
-import com.delivery.model.Customer;
 import com.delivery.model.MenuItem;
-import com.delivery.model.Merchant;
+import com.delivery.model.Order;
+import com.delivery.model.OrderItem;
+import com.delivery.model.OrderState;
 import com.delivery.model.User;
 import com.delivery.services.IMenuService;
 import com.delivery.services.IOrderService;
 import com.delivery.services.IUserService;
+import com.delivery.services.strategy.PercentageDiscount;
 
 public class AppConsole {
     private final IOrderService orderService;
@@ -36,7 +39,7 @@ public class AppConsole {
             System.out.println("   HỆ THỐNG QUẢN LÝ GIAO ĐỒ ĂN  ");
             System.out.println("=========================================");
             System.out.println("1. Vào phân hệ KHÁCH HÀNG (Customer)");
-            System.out.println("2. Vào phân hệ ĐỐI TÁC / CHỦ QUÁN (Merchant)");
+            System.out.println("2. Vào phân hệ Admin cửa hàng");
             System.out.println("3. Tạo tài khoản mới.");
             System.out.println("0. Thoát hệ thống");
             System.out.println("-----------------------------------------");
@@ -48,8 +51,7 @@ public class AppConsole {
                     handleCustomer();
                     break;
                 case 2:
-                    // MerchantConsole merchantConsole = new MerchantConsole(input, orderService);
-                    // merchantConsole.displayMenu();
+                    // TODO: handleAdmin
                     break;
                 case 3:
                     handleCreateUser();
@@ -74,10 +76,7 @@ public class AppConsole {
                     case 1:
                         List<User> users = userService.getAllUser();
                         for (User user : users) {
-                            if (user instanceof Customer) {
-                                user.getDetails();
-                            }
-                            continue;
+                            user.getDetails();
                         }
                         break;
                     case 2:
@@ -107,7 +106,8 @@ public class AppConsole {
                 System.out.println("2. Thêm món ăn vào Giỏ hàng nháp");
                 System.out.println("3. Xem giỏ hàng & Tiến hành thanh toán (Checkout)");
                 System.out.println("4. Viết đánh giá / Chấm sao đơn hàng");
-                System.out.println("5. Đăng xuất (Quay lại màn hình chính)");
+                System.out.println("5. Lịch sử đơn hàng");
+                System.out.println("6. Đăng xuất (Quay lại màn hình chính)");
                 System.out.println("====================================");
                 int input = readIntInput();
                 switch (input) {
@@ -119,8 +119,17 @@ public class AppConsole {
                         addFoodToCart();
                         break;
                     case 3:
-                        checkoutCart();
+                        performCheckout();
                         break;
+                    case 4:
+                        reviewOrder();
+                        break;
+                    case 5:
+                        viewOrderHistory();
+                        break;
+                    case 6:
+                        currentUser = null;
+                        start();
                     default:
                         break;
                 }
@@ -131,85 +140,134 @@ public class AppConsole {
     }
 
     private void addFoodToCart() {
-        var users = userService.getAllUser();
-        for(User user : users){
-            if(user instanceof Merchant){
-                System.out.println(user.getDetails());
-            }
-        }
-        String merchant = readStringInput("Select merchant");
         System.out.println("======MENU======");
         var allItems = menuService.getAllMenuItem();
-        if(allItems.isEmpty()){
+        if (allItems.isEmpty()) {
             System.out.println("Chưa có món nào");
             return;
         }
-        for(MenuItem item: allItems){
-            if(item.getMerchantId()!=merchant){
-                continue;
-            }
-            System.out.println(item.getDetailDescription());
-        }
+
         boolean addFood = true;
         Cart cart = new Cart();
         while (addFood) {
-           try {
-            String endOrder = readStringInput("Complete order(Yes to break)?");
-            if(endOrder.toLowerCase()=="yes"||endOrder.toLowerCase()=="y"){
-                addFood =false;
-                ((Customer)currentUser).setCart(cart);
-                continue;
+            try {
+                String endOrder = readStringInput("Complete order(Yes to break)?");
+                if (endOrder.toLowerCase() == "yes" || endOrder.toLowerCase() == "y") {
+                    addFood = false;
+                    currentUser.setCart(cart);
+                    continue;
+                }
+                String itemId = readStringInput("Enter menu item id to add: ");
+                MenuItem item = menuService.getMenuItemById(itemId);
+                int quantity = readNonIntInput("Enter quantity: ");
+
+                cart.addItem(item, quantity);
+            } catch (Exception e) {
+                System.out.println(e.getMessage());
             }
-            String itemId = readStringInput("Enter menu item id to add: ");
-            MenuItem item = menuService.getMenuItemById(itemId);
-            int quantity = readNonIntInput("Enter quantity: ");
-            
-            cart.addItem(item, quantity);
-           } catch (Exception e) {
-            System.out.println(e.getMessage());
-           }
-                        
+
         }
     }
 
-    private void checkoutCart(){
+    private void performCheckout() {
         try {
-            String orderId = readStringInput("Enter order id: ");
+            var cart = currentUser.getCart();
+            if (cart.getItems().isEmpty()) {
+                System.out.println("Cart is empty!");
+                return;
+            }
+            double subTotal = 0;
+            for (OrderItem item : cart.getItems()) {
+                double itemPrice = item.calculateItemPrice();
+                subTotal += itemPrice;
+                System.out.printf("+ %s x %d = %,.0f VNĐ\n", item.getMenuItem().getName(), item.getQuantity(),
+                        itemPrice);
+            }
+            double shippingFee = 15000; // Mặc định phí ship hệ thống
+            System.out.printf("Tạm tính tiền hàng: %,.0f VNĐ | Phí giao hàng: %,.0f VNĐ\n", subTotal, shippingFee);
 
+            String confirm = readStringInput(
+                    "Bạn có muốn chốt đơn thanh toán không? (Y/Yes or select any key to continue): ");
+            if (!"Y".equalsIgnoreCase(confirm.toUpperCase()) || !"YES".equalsIgnoreCase(confirm.toUpperCase())) {
+                System.out.println("Đã hủy quy trình thanh toán. Giỏ hàng được giữ nguyên.");
+                return;
+            }
 
-        } catch (Exception e) {
-            // TODO: handle exception
+            String orderId = "ORD" + (System.currentTimeMillis() % 100000);
+
+            // TODO: handle to apply discount
+            var promotion = new PercentageDiscount(0.1, 10000);
+            Order order = orderService.checkout(orderId, currentUser.getId(), currentUser.getCart(), promotion,
+                    shippingFee);
+
+            System.out.println("\n=========================================");
+            System.out.println("   🎉 ĐẶT ĐỒ ĂN THÀNH CÔNG (CHECKOUT OK)  ");
+            System.out.println("=========================================");
+            System.out.println("Mã đơn hàng của bạn: " + order.getOrderId());
+            System.out.printf("Số tiền được giảm giá:  -%,.0f VNĐ\n", order.getDiscount());
+            System.out.printf("TỔNG TIỀN ĐÃ KHẤU TRỪ VÍ: %,.0f VNĐ\n", order.getTotalPrice());
+            System.out.println("Trạng thái hiện tại:     " + order.getState().name());
+            System.out.println("=========================================");
+
+        } catch (ValidationException e) {
+            System.out.println("\n[GIAO DỊCH THẤT BẠI] Lỗi xử lý từ hệ thống: " + e.getMessage());
         }
     }
+
+    private void reviewOrder() {
+        System.out.println("---ĐÁNH GIÁ ĐƠN HÀNG ---");
+        String orderId = readStringInput("Enter Order id input to review: ");
+        try {
+            Order order = orderService.getOrderById(orderId);
+            if (order.getState() != OrderState.DELIVERED) {
+                System.out.println("[LỖI] Đơn hàng chưa được giao thành công, không thể để lại đánh giá!");
+                return;
+            }
+            int rating = readNonIntInput("Chấm điểm uy tín (Nhập từ 1 đến 5 Sao): ");
+            while (rating > 5 || rating <= 0) {
+                System.out.println("Nhập số sao sai!");
+                rating = readNonIntInput("Nhập điểm(1--5): ");
+            }
+            String comment = readStringInput("Viết nội dung bình luận nhận xét: ");
+            orderService.submitReview(orderId, rating, comment);
+            System.out.println("[OK] Cảm ơn ý kiến đóng góp của bạn! Điểm uy tín của quán đã được cập nhật.");
+        } catch (NotFoundException e) {
+            System.out.println("[LỖI] Không tìm thấy mã đơn hàng này trên hệ thống!");
+        } catch (ValidationException e) {
+            System.out.println("[LỖI] Đánh giá thất bại: " + e.getMessage());
+        }
+    }
+
+    private void viewOrderHistory() {
+        System.out.println("\n--- LỊCH SỬ ĐƠN HÀNG CỦA BẠN ---");
+        var allOrders = orderService.getAllOrders();
+        boolean hasOrder = false;
+
+        for (Order order : allOrders) {
+            // Lọc đúng mã ID của khách hiện tại
+            if (order.getCustomer().getId().equals(currentUser.getId())) {
+                hasOrder = true;
+                System.out.printf("Đơn: %-8s | Tổng thanh toán: %, -10.0f VNĐ | Trạng thái: %-10s | Đánh giá: %d Sao\n",
+                        order.getOrderId(), order.getTotalPrice(), order.getState().name(), order.getRating());
+            }
+        }
+        if (!hasOrder) {
+            System.out.println("Bạn chưa thực hiện đơn đặt hàng nào trên hệ thống.");
+        }
+    }
+
     // ============================================
     private void handleCreateUser() {
         try {
-            User user = readUserFromInput();
+            String id = readStringInput("User input: ");
+            String name = readStringInput("User name: ");
+            String phoneNumber = readStringInput("User Phone number: ");
+            String address = readStringInput("User Address");
+            User user = new User(id, name, phoneNumber, 100000, address);
             userService.registerUser(user);
             System.out.println("Register new User Successfully");
         } catch (Exception exception) {
             System.out.println("ERROR: " + exception.getMessage());
-        }
-    }
-
-    private User readUserFromInput() {
-        System.out
-                .println("Select type user create:\n1.Customer(Khách hàng)\n2.Merchant(Chủ quán/Đối tác)\n0.Quay lại");
-        System.out.println("Type: ");
-        int typeChoice = readIntInput();
-
-        String id = readStringInput("User input: ");
-        String name = readStringInput("User name: ");
-        String phoneNumber = readStringInput("User Phone number: ");
-        switch (typeChoice) {
-            case 1:
-                String address = readStringInput("User Address");
-                return new Customer(id, name, phoneNumber, 100000, address);
-            case 2:
-                String storeName = readStringInput("User address: ");
-                return new Merchant(id, name, phoneNumber, storeName);
-            default:
-                throw new IllegalArgumentException("Unsupport this User, Try Again!");
         }
     }
 
--- src/main/java/com/delivery/ui/Main.java
+++ src/main/java/com/delivery/ui/Main.java
@@ -1,9 +1,23 @@
 package com.delivery.ui;
 
+import java.nio.file.Path;
+
+import com.delivery.model.User;
+import com.delivery.repository.GenericsRepository;
+import com.delivery.repository.IRepository;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     static void main() {
-      // AppConsole app = new AppConsole(null)
+        Path userDataFilePath = Path.of("data", "users.csv");
+        Path menuDataFilePath = Path.of("data", "menu.csv");
+        Path orderDataFilePath = Path.of("data", "orders.csv");
+        IRepository<User, String> userRepository = new GenericsRepository<>() {
+            @Override
+            protected String getId(User entity) {
+                return entity.getId();
+            }
+        };
     }
 }
